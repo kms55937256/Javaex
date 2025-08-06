4a7964a6f77b45de2caebbcc5d1ca693fc162ebe
kms55937256 <kms55937256@gmail.com>
2025-08-06 18:43:02 +0900

    Javaex 과제1

---
+++ src/mylab/student/control/StudentTest.java
@@ -0,0 +1,22 @@
+package mylab.student.control;
+
+import mylab.student.entity.Student;
+
+import mylab.student.exception.InvalidGradeException;
+
+public class StudentTest {
+	
+	public static void main(String[] args) {
+        try {
+            Student s1 = new Student("2023001", "김민수", "컴퓨터공학", 3);
+            System.out.println(s1.getName() + " / " + s1.getMajor() + " / " + s1.getGrade() + "학년");
+
+            System.out.println("5학년으로 변경");
+            s1.setGrade(5); 
+
+        } catch (InvalidGradeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
